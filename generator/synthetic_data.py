ea652975e229ce56ff578bf6d61bb7e5ede91df5
jist <95856749+george0st@users.noreply.github.com>
2023-10-30 17:49:17 +0100

    Initial basic-partyrelation

--- generator/synthetic_data.py
+++ generator/synthetic_data.py
@@ -4,6 +4,7 @@ import time
 
 from generator.basic_party import BasicParty
 from generator.basic_partycontact import BasicPartyContact
+from generator.basic_partyrelation import BasicPartyRelation
 from generator.basic_account import BasicAccount
 from generator.basic_transaction import BasicTransaction
 from generator.base import Base
@@ -25,6 +26,7 @@ class SyntheticData:
 
         self._create(BasicParty(self._model_path, self._gmodel))
         self._create(BasicPartyContact(self._model_path, self._gmodel))
+        self._create(BasicPartyRelation(self._model_path, self._gmodel))
         self._create(BasicAccount(self._model_path, self._gmodel))
         self._create(BasicTransaction(self._model_path, self._gmodel))
 
