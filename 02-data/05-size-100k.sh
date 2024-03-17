0aee790f07827c4d8cb6fb7f28133b6774cc38db
jist <95856749+george0st@users.noreply.github.com>
2024-03-17 13:12:30 +0100

    Update 05-size-100k.sh

--- 02-data/05-size-100k.sh
+++ 02-data/05-size-100k.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# size 10k with 2k bundles
-python main.py generate -l 2-size-10K -c 10000 -b 2000
+# size 100k with 2k bundles
+python main.py generate -l 2-size-100K -c 100000 -b 2000
 
 # or
-# size 10k with smaller 1k bundles
-# python main.py generate -l 2-size-10K -c 10000 -b 1000
+# size 100k with smaller 3k bundles
+# python main.py generate -l 2-size-100K -c 100000 -b 3000
