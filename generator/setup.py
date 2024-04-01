c20826c967e46ebc5df0095478e5634b40b6f84b
jist <95856749+george0st@users.noreply.github.com>
2024-04-01 14:59:19 +0200

    ...

--- generator/setup.py
+++ generator/setup.py
@@ -5,4 +5,10 @@ import os
 import numpy as np
 
 class Setup():
-    pass
+
+    def __init__(self, model_path):
+        # get setup from json files
+        path=os.path.join(model_path, "model.json")
+
+        with open(path, "r") as json_file:
+            definition = json.load(json_file)
