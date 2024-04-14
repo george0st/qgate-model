a1bd98faf00062da717078e06725fd4394ebd378
jist <95856749+george0st@users.noreply.github.com>
2024-04-14 14:22:43 +0200

    Update base_data.py

--- generator/base_data.py
+++ generator/base_data.py
@@ -44,35 +44,47 @@ class BaseData(Base):
 
         # print(f"Creating: {'APPEND' if append else 'WRITE'}, name: '{self.name}', dir: '{dir}'...")
         df=pd.DataFrame(self.model)
-        if compress:
-            compression_opts = dict(method='gzip')
-            df.to_csv(os.path.join(path,f"{self.name}.csv.gz"),
-                      header=False if append else True,
-                      index=False,
-                      mode="a" if append else "w",
-                      encoding='utf-8',
-                      sep=setup.csv_separator,
-                      decimal=setup.csv_decimal,
-                      compression=compression_opts)
+        compression_opts = dict(method='gzip') if compress else None
+        output_name = f"{self.name}.csv.gz" if compress else f"{self.name}.csv"
+        
+        df.to_csv(os.path.join(path, output_name),
+                  header=False if append else True,
+                  index=False,
+                  mode="a" if append else "w",
+                  encoding='utf-8',
+                  sep=setup.csv_separator,
+                  decimal=setup.csv_decimal,
+                  compression=compression_opts)
 
-#            compression: CompressionOptions = "infer",
-
-
-        #             df.to_parquet(os.path.join(path,f"{self.name}.parquet"),
-#                           engine="pyarrow",
-#                           compression='gzip',
-# #                          header=False if append else True,
-#                           index=False,
-#                           mode = "a" if append else "w")
-
-        else:
-            df.to_csv(os.path.join(path,f"{self.name}.csv"),
-                      header=False if append else True,
-                      index=False,
-                      mode="a" if append else "w",
-                      encoding='utf-8',
-                      sep=setup.csv_separator,
-                      decimal=setup.csv_decimal)
+#         if compress:
+#             compression_opts = dict(method='gzip')
+#             df.to_csv(os.path.join(path,f"{self.name}.csv.gz"),
+#                       header=False if append else True,
+#                       index=False,
+#                       mode="a" if append else "w",
+#                       encoding='utf-8',
+#                       sep=setup.csv_separator,
+#                       decimal=setup.csv_decimal,
+#                       compression=compression_opts)
+#
+# #            compression: CompressionOptions = "infer",
+#
+#
+#         #             df.to_parquet(os.path.join(path,f"{self.name}.parquet"),
+# #                           engine="pyarrow",
+# #                           compression='gzip',
+# # #                          header=False if append else True,
+# #                           index=False,
+# #                           mode = "a" if append else "w")
+#
+#         else:
+#             df.to_csv(os.path.join(path,f"{self.name}.csv"),
+#                       header=False if append else True,
+#                       index=False,
+#                       mode="a" if append else "w",
+#                       encoding='utf-8',
+#                       sep=setup.csv_separator,
+#                       decimal=setup.csv_decimal)
 
             # df.to_parquet(os.path.join(path,f"{self.name}.parquet"),
             #                            engine='fastparquet',
