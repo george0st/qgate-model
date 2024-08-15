5a364650b9a591e5366706a9d0376804cdbac718
jist <95856749+george0st@users.noreply.github.com>
2024-08-15 16:46:42 +0200

    Update basic_relation.py

--- generator/basic_relation.py
+++ generator/basic_relation.py
@@ -30,7 +30,7 @@ class BasicRelation(BaseData):
         if len(parties) < BasicRelation.MAX_RELATIONS:
             from colorama import Fore, Style
 
-            print(Fore.BLUE + f"NOTE: Small amount of parties." + Style.RESET_ALL)
+            print(Fore.BLUE + "      NOTE: Small amount of parties." + Style.RESET_ALL)
             return
 
         # iteration cross all parties
